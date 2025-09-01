c742072515589479ae9e681b8baf3cf7b1240396
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-09-01 13:00:50 +0530

    chore: installed dotenv and created env file and imported it in server js file

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
+require('dotenv').config()
 const indexRouter = require('./routes/indexRouter');
 const newRouter = require("./routes/newRouter.js")
+
 const app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.use("/",indexRouter);
 app.use("/new",newRouter)
 
-const PORT  = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (error) => {
-  // This is important!
-  // Without this, any startup errors will silently fail
-  // instead of giving you a helpful error message.
   if (error) {
     throw error;
   }
