976935d6b6edcca16739228d40aa074b376d6477
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-09-09 19:35:42 +0530

    chore: add server side validation for the user input

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,22 +1,54 @@
+
 const db = require("../db/queries");
+const { body, validationResult } = require('express-validator');
+
+const validateUser = [
+  body("name").isLength({ min: 2, max: 20 }).withMessage("Length of name should be between 2 to 20 characters").isAlpha("en-US", { ignore: " " }).withMessage("Name shouldnt have number in it"),
+  body("message").isLength({min:5,max:40}).withMessage("Length of the message should be between 5 to 40 characters")
+];
 
 function renderForm(req, res) {
   res.render("form", {});
 }
 
-async function postMessage(req, res) {
-  const message = req.body.message;
-  const name = req.body.name;
+const postMessage = [
+  validateUser, async(req,res) => {
+    const errors = validationResult(req);
+    const message = req.body.message;
+    const name = req.body.name;
 
-  const messageObject = {
+      const messageObject = {
     text: message,
     username: name,
     added: new Date(),
   };
 
-  await db.postMessageToDb(messageObject);
+  if(!errors.isEmpty()) {
+    
+    return res.status(400).render("form",{errors : errors.array(),name:name,message:message})
+  }
 
-  res.redirect("/");
-}
+  else if(errors.isEmpty()) {
+    await db.postMessageToDb(messageObject);
+    return res.render("index",{});
+  }
+
+  }
+]
+
+// async function postMessage(req, res) {
+//   const message = req.body.message;
+//   const name = req.body.name;
+
+//   const messageObject = {
+//     text: message,
+//     username: name,
+//     added: new Date(),
+//   };
+
+//   await db.postMessageToDb(messageObject);
+
+//   res.redirect("/");
+// }
 
 module.exports = { renderForm, postMessage };
