c4eaa6a32b211c0d9558c94129928edf730de6b8
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-09-09 22:52:41 +0530

    feat: add styling to make mobile responsive webpage

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,10 +1,15 @@
-
 const db = require("../db/queries");
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
 const validateUser = [
-  body("name").isLength({ min: 2, max: 20 }).withMessage("Length of name should be between 2 to 20 characters").isAlpha("en-US", { ignore: " " }).withMessage("Name shouldnt have number in it"),
-  body("message").isLength({min:5,max:40}).withMessage("Length of the message should be between 5 to 40 characters")
+  body("name")
+    .isLength({ min: 2, max: 20 })
+    .withMessage("Length of name should be between 2 to 20 characters")
+    .isAlpha("en-US", { ignore: " " })
+    .withMessage("Name shouldnt have number in it"),
+  body("message")
+    .isLength({ min: 5, max: 165 })
+    .withMessage("Length of the message should be between 5 to 165 characters"),
 ];
 
 function renderForm(req, res) {
@@ -12,43 +17,31 @@ function renderForm(req, res) {
 }
 
 const postMessage = [
-  validateUser, async(req,res) => {
+  validateUser,
+  async (req, res) => {
     const errors = validationResult(req);
     const message = req.body.message;
     const name = req.body.name;
 
-      const messageObject = {
-    text: message,
-    username: name,
-    added: new Date(),
-  };
+    const messageObject = {
+      text: message,
+      username: name,
+      added: new Date(),
+    };
 
-  if(!errors.isEmpty()) {
-    
-    return res.status(400).render("form",{errors : errors.array(),name:name,message:message})
-  }
-
-  else if(errors.isEmpty()) {
-    await db.postMessageToDb(messageObject);
-    return res.render("index",{});
-  }
-
-  }
-]
-
-// async function postMessage(req, res) {
-//   const message = req.body.message;
-//   const name = req.body.name;
-
-//   const messageObject = {
-//     text: message,
-//     username: name,
-//     added: new Date(),
-//   };
-
-//   await db.postMessageToDb(messageObject);
-
-//   res.redirect("/");
-// }
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .render("form", {
+          errors: errors.array(),
+          name: name,
+          message: message,
+        });
+    } else if (errors.isEmpty()) {
+      await db.postMessageToDb(messageObject);
+      return res.render("index", {});
+    }
+  },
+];
 
 module.exports = { renderForm, postMessage };
